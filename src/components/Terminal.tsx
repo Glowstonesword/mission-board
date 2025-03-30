515e5cd43e9f83271549a9537cfe6aea17171231
Glowstonesword <sam.wadey@hotmail.com>
2025-03-30 12:27:46 +0100

    Update Terminal.tsx

--- src/components/Terminal.tsx
+++ src/components/Terminal.tsx
@@ -41,7 +41,7 @@ export const commands: Command = [
   { cmd: "themes", desc: "check available themes", tab: 7 },
   { cmd: "welcome", desc: "display hero section", tab: 6 },
   { cmd: "whoami", desc: "about current user", tab: 7 },
-  { cmd: "test", desc: "teeeesting!", tab: 4 },
+  { cmd: "test", desc: "teeeesting!", tab: 9 },
 ];
 
 type Term = {
