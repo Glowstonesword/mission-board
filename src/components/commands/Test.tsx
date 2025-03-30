b3452fd2b9a6de076c80969b57c980ab59c9cc76
Glowstonesword <sam.wadey@hotmail.com>
2025-03-30 12:28:55 +0100

    Update Test.tsx

--- src/components/commands/Test.tsx
+++ src/components/commands/Test.tsx
@@ -1,7 +1,7 @@
 import { EduIntro, EduList } from "../styles/Education.styled";
 import { Wrapper } from "../styles/Output.styled";
 
-const Education: React.FC = () => {
+const Test: React.FC = () => {
   return (
     <Wrapper data-testid="education">
       <EduIntro>Here is my meme background!</EduIntro>
@@ -30,4 +30,4 @@ const eduBg = [
   },
 ];
 
-export default Education;
+export default Test;
