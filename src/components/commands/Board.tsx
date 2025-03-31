bd9f1f31c3d628e5e9ef091d978cf187b24fc251
Glowstonesword <sam.wadey@hotmail.com>
2025-03-31 17:15:45 +0100

    Update Board.tsx

--- src/components/commands/Board.tsx
+++ src/components/commands/Board.tsx
@@ -8,37 +8,8 @@ const Board: React.FC = () => {
       <EduIntro>==============</EduIntro>
       <EduList>TEST BOI</EduList>
       <EduList>Test2</EduList>
-      {eduBg.map(({ title, source, intro, line }) => (
-        <EduList key={title}>
-          <div className="title">{title}</div>
-          <div className="source">{source}</div>
-          <div className="intro">{intro}</div>
-          <div className="line">{line}</div>
-        </EduList>
-      ))}
     </Wrapper>
   );
 };
-
-const eduBg = [
-  {
-    title: "What Goes Around",
-    source: "MISSION AVAILABLE - DIRECTLY FORWARDED [OPEN CONTRACTS > IPS-N]",
-    intro: "Test",
-    line: " ",
-  },
-  {
-    title: "Something Or Other",
-    source: "MISSION AVAILABLE - OPEN CHANNEL [OPEN CONTRACTS > IPS-N]",
-    intro: "Test  test2",
-    line: " ",
-  },
-  {
-    title: "Who Watches The Watchers",
-    source: "MISSION AVAILABLE - CLOSED CHANNEL [CLOSED CONTRACT > SSF]",
-    intro: "Flock of Iron. This is Lieutenant Bridges.",
-    line: " ",
-  },
-];
 
 export default Board;
