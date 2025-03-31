55863e40cf84252fe5315803868e951d254c6a48
Glowstonesword <sam.wadey@hotmail.com>
2025-03-31 16:58:45 +0100

    Update Board.tsx

--- src/components/commands/Board.tsx
+++ src/components/commands/Board.tsx
@@ -6,11 +6,21 @@ const Board: React.FC = () => {
     <Wrapper data-testid="education">
       <EduIntro><b>MISSION BOARD</b></EduIntro>
       <EduIntro>==============</EduIntro>
-      {eduBg.map(({ title, source, desc, line }) => (
+      {eduBg.map(({ title, source, desc, para1, para2, para3, para4, para5, para6, para7, para8, para9, para10, line }) => (
         <EduList key={title}>
           <div className="title">{title}</div>
           <div className="source">{source}</div>
-          <div className="desc">{desc}</div>
+          <div className="intro">{intro}</div>
+          <div className="para1">{para1}</div>
+          <div className="para2">{para2}</div>
+          <div className="para3">{para3}</div>
+          <div className="para4">{para4}</div>
+          <div className="para5">{para5}</div>
+          <div className="para6">{para6}</div>
+          <div className="para7">{para7}</div>
+          <div className="para8">{para8}</div>
+          <div className="para9">{para9}</div>
+          <div className="para10">{para10}</div>
           <div className="line">{line}</div>
         </EduList>
       ))}
@@ -22,19 +32,19 @@ const eduBg = [
   {
     title: "What Goes Around",
     source: "MISSION AVAILABLE - DIRECTLY FORWARDED [OPEN CONTRACTS > IPS-N]",
-    desc: Test,
+    intro: "Test",
     line: " ",
   },
   {
     title: "Something Or Other",
     source: "MISSION AVAILABLE - OPEN CHANNEL [OPEN CONTRACTS > IPS-N]",
-    desc: "Test",
+    intro: "Test", "test",
     line: " ",
   },
   {
     title: "Who Watches The Watchers",
     source: "MISSION AVAILABLE - CLOSED CHANNEL [CLOSED CONTRACT > SSF]",
-    desc: "Flock of Iron. This is Lieutenant Bridges."
+    intro: "Flock of Iron. This is Lieutenant Bridges.",
     line: " ",
   },
 ];
