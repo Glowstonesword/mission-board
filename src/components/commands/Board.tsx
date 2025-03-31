564f06381b3bcff3879f66ab9ff99bb74d33f828
Glowstonesword <sam.wadey@hotmail.com>
2025-03-31 17:49:42 +0100

    Update Board.tsx

--- src/components/commands/Board.tsx
+++ src/components/commands/Board.tsx
@@ -5,11 +5,12 @@ const Board: React.FC = () => {
   return (
     <Wrapper data-testid="education">
       <EduIntro><b>MISSION BOARD</b><p>=============</p></EduIntro>
-      <EduIntro><b>What Goes Around</b><p>MISSION AVAILALE - CLOSED CHANNEL <b>[DIRECT REQUEST &gt; SSC]</b></p><p>Start</p></EduIntro>
+      <EduIntro><b>What Goes Around</b><p>MISSION AVAILALE - CLOSED CHANNEL <b>[DIRECT REQUEST &gt; SSC]</b></p></EduIntro>
+      <EduIntro>Ordoesthislookbetterretgsetrghsthsrdhdrhtdrhdrtyhdytrh</EduIntro>
       <EduIntro>Continue</EduIntro>
       <EduIntro>Finish</EduIntro>
       <EduList>‎ </EduList>
-      <EduIntro><b>Who Investigates The Investigators</b><p>MISSION AVAILALE - CLOSED CHANNEL <b>[DIRECT REQUEST &gt; SSF]</b></p><p>Start</p></EduIntro>
+      <EduIntro><b>Who Investigates The Investigators</b><p>MISSION AVAILALE - CLOSED CHANNEL <b>[DIRECT REQUEST &gt; SSF]</b></p><p>Start garefgasergsrehgsrhtshshtsdrhshsrhtsrhsrhsrhs</p></EduIntro>
       <EduIntro>Continue</EduIntro>
       <EduIntro>Finish</EduIntro>
     </Wrapper>
