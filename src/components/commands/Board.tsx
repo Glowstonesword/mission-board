ac92bbd328901dc1560386981d51e21995400cbe
Glowstonesword <sam.wadey@hotmail.com>
2025-03-31 17:38:25 +0100

    Update Board.tsx

--- src/components/commands/Board.tsx
+++ src/components/commands/Board.tsx
@@ -5,7 +5,7 @@ const Board: React.FC = () => {
   return (
     <Wrapper data-testid="education">
       <EduIntro><b>MISSION BOARD</b><p>=============</p></EduIntro>
-      <EduIntro>CRITICAL MISSION AVAILABLE - CLOSED CHANNEL <b>[DIRECT REQUEST > SSF]</b><p>Start</p></EduIntro>
+      <EduIntro>CRITICAL MISSION AVAILABLE - CLOSED CHANNEL <b>[DIRECT REQUEST &gt; SSF]</b><p>Start</p></EduIntro>
       <EduIntro>Continue</EduIntro>
       <EduIntro>Finish</EduIntro>
       <EduList>‎ </EduList>
