9c8fd64d24cac7e23454bdef47894bbabe5e6210
Glowstonesword <sam.wadey@hotmail.com>
2025-04-01 09:58:20 +0100

    Update Contacts.tsx

--- src/components/commands/Contacts.tsx
+++ src/components/commands/Contacts.tsx
@@ -4,13 +4,18 @@ import { Wrapper } from "../styles/Output.styled";
 const Contacts: React.FC = () => {
   return (
     <Wrapper data-testid="education">
-      <EduIntro><b>SSF</b><p>=============</p></EduIntro>
-      <EduIntro><b>CPT. FERRA AUTUMN</b><p>SSF CAPTAIN</p></EduIntro>
-      <EduIntro>Captain Ferra Autumn is the captain of the local forces of the Scrapyard Security Force.</EduIntro>
+      <EduIntro><b>SSF</b><p>SCRAPHEAP SECURITY FORCE</p></p><p>=============</p></EduIntro>
+      <EduIntro><b>CPT. FERRA AUTUMN</b><p>OH CAPTAIN MY CAPTAIN</p></EduIntro>
+      <EduIntro><b>CAPTAIN FERRA AUTUMN</b> is the captain of the local forces of the <b>SCRAPYARD SECURITY FORCE</b>.</EduIntro>
       <EduList>‎ </EduList>
-      <EduIntro><b>LT. RHYS BRIDGES</b><p>SSF LIEUTENANT</p></EduIntro>
+      <EduIntro><b>LT. RHYS BRIDGES</b><p>OL' RELIABLE</p></EduIntro>
       <EduIntro>A lieutenant serving under <b>CPT. FERRA AUTUMN</b>. Frequent contact and handler for the <b>FLOCK OF IRON</b> in their contracts with the <b>SSF</b></EduIntro>
       <EduList>‎ </EduList>
+      <EduIntro><b>TCH. WINNA ROUGE</b><p>IMPROVISED ENGINEERING</p></EduIntro>
+      <EduIntro>Working to keep things running smoothly at the <b>GIBRALTAR COMPLEX</b>, <b>TECHNICIAN WINNA ROUGE</b> is the port of call to the <b>FLOCK OF IRON</b> for their material needs from the <b>SSF</b></EduIntro>
+      <EduList>‎ </EduList>
+      <EduList>‎ </EduList>
+      <EduIntro><b>UNCLIPPED WINGS</b><p>RISING RAGTAG REVOLUTIONARIES</p><p>=============</p></EduIntro>
     </Wrapper>
   );
 };
