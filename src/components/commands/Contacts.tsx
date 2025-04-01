555ea9df55c639b270706d04e84f4224fa0879b9
Glowstonesword <sam.wadey@hotmail.com>
2025-04-01 09:50:01 +0100

    Update Contacts.tsx

--- src/components/commands/Contacts.tsx
+++ src/components/commands/Contacts.tsx
@@ -4,7 +4,13 @@ import { Wrapper } from "../styles/Output.styled";
 const Contacts: React.FC = () => {
   return (
     <Wrapper data-testid="education">
-      <EduIntro>Contacts will be here.</EduIntro>
+      <EduIntro><b>SSF</b><p>=============</p></EduIntro>
+      <EduIntro><b>CPT. FERRA AUTUMN</b><p>SSF CAPTAIN</p></EduIntro>
+      <EduIntro>Captain Ferra Autumn is the captain of the local forces of the Scrapyard Security Force.</EduIntro>
+      <EduList>‎ </EduList>
+      <EduIntro><b>LT. RHYS BRIDGES</b><p>SSF LIEUTENANT</p></EduIntro>
+      <EduIntro>A lieutenant serving under <b>CPT. FERRA AUTUMN</b>. Frequent contact and handler for the <b>FLOCK OF IRON</b> in their contracts with the <b>SSF</b></EduIntro>
+      <EduList>‎ </EduList>
     </Wrapper>
   );
 };
