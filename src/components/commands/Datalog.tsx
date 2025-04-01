bf8bd8e549933ca5376448401a9781a95d898c8a
Glowstonesword <sam.wadey@hotmail.com>
2025-04-01 06:52:23 +0100

    Update Datalog.tsx

--- src/components/commands/Datalog.tsx
+++ src/components/commands/Datalog.tsx
@@ -4,30 +4,9 @@ import { Wrapper } from "../styles/Output.styled";
 const Datalog: React.FC = () => {
   return (
     <Wrapper data-testid="education">
-      <EduIntro>Here is my meme background!</EduIntro>
-      {eduBg.map(({ title, desc }) => (
-        <EduList key={title}>
-          <div className="title">{title}</div>
-          <div className="desc">{desc}</div>
-        </EduList>
-      ))}
+      <EduIntro>Datalog will be here.</EduIntro>
     </Wrapper>
   );
 };
-
-const eduBg = [
-  {
-    title: "B.Sc (Hons) in Computing",
-    desc: "Edinburgh Napier University | 2018 ~ 2019",
-  },
-  {
-    title: "HND in Computing & System Development",
-    desc: "Info Myanmar University | 2016 - 2018",
-  },
-  {
-    title: "IELTS 6.5",
-    desc: "British Council Myanmar | 2017",
-  },
-];
 
 export default Datalog;
