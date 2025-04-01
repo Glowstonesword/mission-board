1f9b13c986ec85060884274b0e8990395c12c238
Glowstonesword <sam.wadey@hotmail.com>
2025-04-01 14:08:13 +0100

    Update Projects.tsx

--- src/components/commands/Projects.tsx
+++ src/components/commands/Projects.tsx
@@ -4,7 +4,11 @@ import { Wrapper } from "../styles/Output.styled";
 const Projects: React.FC = () => {
   return (
     <Wrapper data-testid="education">
-      <EduIntro>Ongoing projects will be here.</EduIntro>
+      <EduIntro><b>PROJECTS</b><p>ONGOING GROUP DOWNTIME PROJECTS</p><p>=============</p></EduIntro>
+      <EduIntro><b>SYNAPTIC CONNECTOR</b><p>GREAT MINDS THINK ALIKE</p><p>0/?</p></EduIntro>
+      <EduIntro>Using components from the amalgamated mech wrecks and the <b>NULLSCAPE</b> prototype recovered from a <b>HORUS</b> cult, <b>JACK</b> and <b>KATZEN</b> aim to reinforce their teamwork through a mental connection.</EduIntro>
+      <EduIntro>Grants the benefit of adjacency-based talents from a distance.</EduIntro>
+      <EduList>‎ </EduList>
     </Wrapper>
   );
 };
