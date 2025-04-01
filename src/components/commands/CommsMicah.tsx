2baf6a1a311d2c1b6082952dadc7f8852727b92e
Glowstonesword <sam.wadey@hotmail.com>
2025-04-01 13:54:40 +0100

    Update CommsMicah.tsx

--- src/components/commands/CommsMicah.tsx
+++ src/components/commands/CommsMicah.tsx
@@ -4,7 +4,7 @@ import { Wrapper } from "../styles/Output.styled";
 const CommsMicah: React.FC = () => {
   return (
     <Wrapper data-testid="education">
-      <EduIntro><b>COMMUNICATIONS</b><p>FIELD TEAM ALBATROSS MEMBER 'JAVELIN'</p><p>=============</p></EduIntro>
+      <EduIntro><b>COMMUNICATIONS</b><p>FIELD TEAM ALBATROSS - CALLSIGN 'JAVELIN'</p><p>=============</p></EduIntro>
       <EduIntro>No incoming communications detected.</EduIntro>
     </Wrapper>
   );
