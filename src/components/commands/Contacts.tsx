b443e3da9bfb0991fb05a1ded33dc4e8eda347af
Glowstonesword <sam.wadey@hotmail.com>
2025-04-01 13:46:48 +0100

    Update Contacts.tsx

--- src/components/commands/Contacts.tsx
+++ src/components/commands/Contacts.tsx
@@ -29,13 +29,56 @@ const Contacts: React.FC = () => {
       <EduIntro><b>FORUM ADMIN ???</b><p>3DG3Y A5 FUCK</p></EduIntro>
       <EduIntro>An unknown <b>HORUS</b> member that possibly runs a local sect, found online through the work of <b>KATZEN</b> and <b>KIRCHE</b>. Has given the group information on a quid-pro-quo basis before several times.</EduIntro>
       <EduList>‎ </EduList>
-      <EduIntro><b>THE CULT OF ARTIFICIAL EVOLUTION</b><p>FANATICS WITH A FIGHTPIT</p></EduIntro>
+      <EduIntro><b>THE SECT OF ARTIFICIAL EVOLUTION</b><p>FANATICS WITH A FIGHTPIT</p></EduIntro>
       <EduIntro>A relatively local sect of the <b>CULT OF RA</b>, this group dedicated to the advancement of mech technology through gladiatorial combat in <b>RA</b>'s name. <b>MICAH</b> and <b>JACK</b> have met them in person and received their approval.</EduIntro>
       <EduList>‎ </EduList>
       <EduIntro><b>RANDOM CULT</b><p>WHITE COLLAR SCRIPTKIDDIES</p></EduIntro>
       <EduIntro>While ostensibly acting under <b>HORUS</b>'s name, they're more just a group of small-time financial criminals with a side of protection work. Met and thoroughly intimidated by <b>MICAH</b> and <b>JACK</b> before being brought under the control of the <b>FORUM ADMIN</b>.</EduIntro>
       <EduList>‎ </EduList>
       <EduList>‎ </EduList>
+      <EduList>‎ </EduList>
+      <EduIntro><b>INDEPENDENTS - GIBRALTAR COMPLEX</b><p>CLOSE TO HOME</p><p>=============</p></EduIntro>
+      <EduIntro><b>DODGER AND TROUBLE</b><p>TWIN TERRORS</p></EduIntro>
+      <EduIntro>Two orphans found in the <b>GIBRALTAR COMPLEX</b> by the <b>FLOCK OF IRON</b> during an investigation into a stolen <b>UNION</b> contraband delivery. Looked after by <b>ALBUS</b>, a homeless man living nearby.</EduIntro>
+      <EduList>‎ </EduList>
+      <EduIntro><b>ALBUS</b><p>NO MAGIC HERE</p></EduIntro>
+      <EduIntro>Looks after <b>DODGER</b> and <b>TROUBLE</b>, as well as several other orphans and homeless, in the slum area of the <b>GIBRALTAR COMPLEX</b>. Has since gained greater recognition as a manager of a safe space for the downtrodden since your 'visit' to the slums.</EduIntro>
+      <EduList>‎ </EduList>
+      <EduIntro><b>ALBUS</b><p>NO MAGIC HERE</p></EduIntro>
+      <EduIntro>Looks after <b>DODGER</b> and <b>TROUBLE</b>, as well as several other orphans and homeless, in the slum area of the <b>GIBRALTAR COMPLEX</b>. Has since gained greater recognition as a manager of a safe space for the downtrodden since your 'visit' to the slums.</EduIntro>
+      <EduList>‎ </EduList>
+      <EduIntro><b>AARON</b><p>PROWD OWNER OF AARON'S ARSENAL EMPORIUM</p></EduIntro>
+      <EduIntro>Store manager of the self-titled <b>AARON'S ARSENAL EMPORIUM</b>. Has sold the group supplies and traded information to them. Looking forward to the group's next visit!</EduIntro>
+      <EduList>‎ </EduList>
+      <EduIntro><b>NOAH</b><p>SPONSORED BY THE FLOCK OF IRON</p></EduIntro>
+      <EduIntro>Owns a stall called <b>NOAH'S ARC</b> in the <b>GIBRALTAR COMPLEX</b>, got a photo with the <b>FLOCK OF IRON</b> after selling them information and several useful blueprints.</EduIntro>
+      <EduList>‎ </EduList>
+      <EduIntro><b>GEN13</b><p>HE'LL GRANT YOUR WISHES, FOR A PRICE</p></EduIntro>
+      <EduIntro>Information and tech vendor in the <b>GIBRALTAR COMPLEX</b> slums. Sold the group information on the stolen <b>UNION</b> contraband stash.</EduIntro>
+      <EduList>‎ </EduList>
+      <EduIntro><b>DARREN FOUR</b><p>BIT OF A SQUARE</p></EduIntro>
+      <EduIntro>Leader of the self-titled <b>FOUR GANG</b> in the slums of the <b>GIBRALTAR COMPLEX</b>. Superior of <b>GEN13</b> and has done business with the group before through the shop <b>ERRIC'S EMPORIUM</b>.</EduIntro>
+      <EduList>‎ </EduList>
+      <EduList>‎ </EduList>
+      <EduList>‎ </EduList>
+      <EduIntro><b>INDEPENDENTS - OTHER</b><p>CLOSE TO HOME</p><p>=============</p></EduIntro>
+      <EduIntro><b>ASHER REN</b><p>COMPLETELY LEGAL SHIPPING SOLUTIONS</p></EduIntro>
+      <EduIntro>Long-time friend of <b>MICAH</b>, and a fellow survivor of the destruction of their homeworld, <b>ASHER REN</b> runs a shipping business of questionable legality while looking for more information on the cause of the disaster.</EduIntro>
+      <EduList>‎ </EduList>
+      <EduIntro><b>XIONG XIAOLI</b><p>ONE-PUNCH GAL</p></EduIntro>
+      <EduIntro>Living Lancer legend of the Long Rim and ostensible creator of the <b>ZHENG</b>-pattern frame. Famous for killing 45 opponents in a single sitrep, a task known as the <b>XIAOLI LIMIT</b>. Somehow still alive. Beat the crap out of <b>ODIN</b>; they're cool now. Protects a local settlement on <b>SCRAPHEAP</b>.</EduIntro>
+      <EduList>‎ </EduList>
+      <EduIntro><b>ICARA SUNZI</b><p>WHAT IS THAT MELODY!?</p></EduIntro>
+      <EduIntro>Living, slightly less legendary Lancer than her wife. Possibly the creator of the <b>SUNZI</b> frame, going by her name. Met <b>ODIN</b> when <b>XIAOLI</b> brought him back unconcious after the barfight to give him some tips and tricks. Married happily to <b>XIAOLI</b> and ridiculously good with computers.</EduIntro>
+      <EduList>‎ </EduList>
+      <EduList>‎ </EduList>
+      <EduList>‎ </EduList>
+      <EduIntro><b>UNION</b><p>THE POPO</p><p>=============</p></EduIntro>
+      <EduIntro><b>DR. VANCE REED</b><p>SCIENCE!</p></EduIntro>
+      <EduIntro>Head of NHP Research under <b>THIRD COMM</b> for many years. Has met the group in person during the <b>HELIOS</b> incident and has offered his counsel to the aspiring mercenary company.</EduIntro>
+      <EduList>‎ </EduList>
+      <EduIntro><b>FAR-FIELD TEAM LEAD 'OMICRON'</b><p>EXTREMELY SPOOK-Y</p></EduIntro>
+      <EduIntro>Former team lead of <b>JACK</b> and involved in the <b>BURNING GATE</b> incident. Still keeps in touch and is a point of contact to <b>UNION</b> through <b>JACK</b>.</EduIntro>
       <EduList>‎ </EduList>
     </Wrapper>
   );
