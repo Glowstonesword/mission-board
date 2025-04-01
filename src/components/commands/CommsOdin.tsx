ed3a0cafb4424ec6c455b3482e00193abe0d520d
Glowstonesword <sam.wadey@hotmail.com>
2025-04-01 13:54:15 +0100

    Update CommsOdin.tsx

--- src/components/commands/CommsOdin.tsx
+++ src/components/commands/CommsOdin.tsx
@@ -4,7 +4,8 @@ import { Wrapper } from "../styles/Output.styled";
 const CommsOdin: React.FC = () => {
   return (
     <Wrapper data-testid="education">
-      <EduIntro>Comms will be here.</EduIntro>
+      <EduIntro><b>COMMUNICATIONS</b><p>FIELD TEAM ALBATROSS - CALLSIGN 'CORVUS'</p><p>=============</p></EduIntro>
+      <EduIntro>No incoming communications detected.</EduIntro>
     </Wrapper>
   );
 };
