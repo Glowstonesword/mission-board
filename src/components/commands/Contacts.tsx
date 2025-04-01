c85f20e5ac0f814262362d582b62aa4e33c2a8a3
Glowstonesword <sam.wadey@hotmail.com>
2025-04-01 10:15:19 +0100

    Update Contacts.tsx

--- src/components/commands/Contacts.tsx
+++ src/components/commands/Contacts.tsx
@@ -8,7 +8,7 @@ const Contacts: React.FC = () => {
       <EduIntro><b>CPT. FERRA AUTUMN</b><p>OH CAPTAIN MY CAPTAIN</p></EduIntro>
       <EduIntro><b>CAPTAIN FERRA AUTUMN</b> is the captain of the local forces of the <b>SCRAPYARD SECURITY FORCE</b>.</EduIntro>
       <EduList>‎ </EduList>
-      <EduIntro><b>LT. RHYS BRIDGES</b><p>OL' RELIABLE</p></EduIntro>
+      <EduIntro><b>LT. RHYS BRIDGES</b><p>OLD RELIABLE</p></EduIntro>
       <EduIntro>A lieutenant serving under <b>CPT. FERRA AUTUMN</b>. Frequent contact and handler for the <b>FLOCK OF IRON</b> in their contracts with the <b>SSF</b></EduIntro>
       <EduList>‎ </EduList>
       <EduIntro><b>TCH. WINNA ROUGE</b><p>IMPROVISED ENGINEERING</p></EduIntro>
@@ -20,8 +20,8 @@ const Contacts: React.FC = () => {
       <EduIntro><b>JASPER BLACKTHORNE</b><p>ABSOLUTE UNIT</p></EduIntro>
       <EduIntro>Heading the local division of the <b>UNCLIPPED WINGS</b>, and old friend of <b>CORVUS</b>, <b>JASPER</b> pilots a minigun-wielding <b>GOLIATH</b> in his efforts to bring an end to the corporation's grip on the galaxy.</EduIntro>
       <EduList>‎ </EduList>
-      <EduIntro><b>GWEN CATLOW</b><p>ABSOLUTE UNIT</p></EduIntro>
-      <EduIntro>Second-in-command and quartermaster of the local division of the <b>UNCLIPPED WINGS</b>, <b>GWEN</b> is a young strategic prodigy backing up and being trained by <b>JASPER BLACKTHORNE</b> in their fight.</EduIntro>
+      <EduIntro><b>GWEN CATLOW</b><p>STARSTRUCK STRATEGIST</p></EduIntro>
+      <EduIntro>Second-in-command and quartermaster of the local division of the <b>UNCLIPPED WINGS</b>, <b>GWEN</b> is a young strategic prodigy backing up and being trained by <b>JASPER BLACKTHORNE</b> in their fight. Admires their former savior, <b>ODIN GALLANT</b>.</EduIntro>
       <EduList>‎ </EduList>
       <EduList>‎ </EduList>
       <EduList>‎ </EduList>
