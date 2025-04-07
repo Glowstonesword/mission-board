a15a43ef4e94b52ec22f1c8dd4665ef270ce2bd0
Glowstonesword <sam.wadey@hotmail.com>
2025-04-07 13:56:35 +0100

    Update CommsKatzen.tsx

--- src/components/commands/CommsKatzen.tsx
+++ src/components/commands/CommsKatzen.tsx
@@ -5,7 +5,10 @@ const CommsKatzen: React.FC = () => {
   return (
     <Wrapper data-testid="education">
       <EduIntro><b>COMMUNICATIONS</b><p>FIELD TEAM ALBATROSS - CALLSIGN 'MITTENS'</p><p>=============</p></EduIntro>
-      <EduIntro>No incoming communications detected.</EduIntro>
+      <EduIntro><p>INCOMING COMMUNICATIONS <b>[SENDER: WHISPER]</b><p>Date: From the Unclipped Wings Bar Party</p></p></EduIntro>
+      <EduIntro>fluffy. floof. very floof. you gud person. gib cuddle. not drunk. no. floof.</EduIntro>
+      <EduIntro>MESSAGE ENDS.</EduIntro>
+      <EduList>‎ </EduList>
     </Wrapper>
   );
 };
