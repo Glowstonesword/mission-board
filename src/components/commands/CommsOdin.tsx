47612f42eb42f72661c50cd134b9b799d23b67b7
Glowstonesword <sam.wadey@hotmail.com>
2025-04-09 13:23:50 +0100

    Update CommsOdin.tsx

--- src/components/commands/CommsOdin.tsx
+++ src/components/commands/CommsOdin.tsx
@@ -5,7 +5,10 @@ const CommsOdin: React.FC = () => {
   return (
     <Wrapper data-testid="education">
       <EduIntro><b>COMMUNICATIONS</b><p>FIELD TEAM ALBATROSS - CALLSIGN 'CORVUS'</p><p>=============</p></EduIntro>
-      <EduIntro>No incoming communications detected.</EduIntro>
+      <EduIntro><p>INCOMING COMMUNICATIONS <b>[SENDER: ERROR]</b><p>Date: From the Unclipped Wings Bar Party</p></p></EduIntro>
+      <EduIntro>You two idiots beat each other up thinking each of the other person was me when I stepped out the ring. It was quite funny, though you broke a table. Don't worry, I already got the money out of your accounts. Your insults suck, work on your game. -Xi.</EduIntro>
+      <EduIntro>MESSAGE ENDS.</EduIntro>
+      <EduList>‎ </EduList>
     </Wrapper>
   );
 };
